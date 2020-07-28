be9249ffa825b5e846c44a3d1905d69611ac6a89
abashak <abashak@abisoft.spb.ru>
2020-07-28 14:57:49 +0300

    work: update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 from setuptools import setup, find_packages
-from pathlib import Path
+from pkg_resources import parse_requirements
 
+
+def load_requirements(fname):
+    requirements = []
+    for req in parse_requirements(open(fname, 'r').read()):
+        extras = '[{}]'.format(','.join(req.extras)) if req.extras else ''
+        requirements.append('{}{}{}'.format(req.name, extras, req.specifier))
+    return requirements
 
 setup(
     name='raw_struct',
     version='0.1',
     author='Andrey Bashakov',
     author_email='abashak@abisoft.spb.ru',
+    url='https://github.com/Bashakov/raw_struct',
     packages=find_packages(exclude=['tests']),
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    tests_require=['pytest'],
     setup_requires=['pytest-runner'],
+    install_requires=[],
+    extras_require={'dev': load_requirements('requirements.dev.txt')}
 )
